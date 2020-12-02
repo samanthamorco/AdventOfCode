37549674ec3afcf110650a21d3302ab04ad05636
Sami Morco <samanthamorco@gmail.com>
2020-12-02 14:53:30 -0800

    Initial commit: first 2 days

---
+++ day1/day1.rb
@@ -0,0 +1,15 @@
+nums = File.readlines('nums.txt').map!(&:to_i)
+
+def part_1(nums)
+  nums.each do |num|
+    second_num = 2020 - num
+    return num * second_num if nums.include?(second_num)
+  end
+end
+puts part_1(nums)
+
+def part_2(nums)
+  nums.combination(3).find { |combo| combo.sum == 2020 }.reduce(:*)
+end
+
+puts part_2(nums)
---
+++ day2/main.rb
@@ -0,0 +1,37 @@
+inputs = File.readlines('input.txt').map!(&:split)
+
+def part1(inputs)
+  valid_passwords = 0
+  inputs.each do |input|
+    p input
+
+    char_range = input[0].split('-')
+    char = input[1].gsub(':', '')
+    char_count = input[2].count(char)
+    puts(char_range[0].to_i <= char_count && char_count <= char_range[1].to_i)
+    valid_passwords += 1 if char_range[0].to_i <= char_count && char_count <= char_range[1].to_i
+  end
+
+  valid_passwords
+end
+
+def part2(inputs)
+  valid_passwords = 0
+
+  inputs.each do |input|
+    char_range = input[0].split('-').map!(&:to_i)
+    char = input[1].gsub(':', '')
+    password = input[2]
+
+    if (password[char_range[0] - 1] == char || password[char_range[1] - 1] == char)
+      next if (password[char_range[0] - 1] == char && password[char_range[1] - 1] == char)
+
+      valid_passwords += 1
+    end
+  end
+
+  valid_passwords
+end
+
+puts part1(inputs)
+puts part2(inputs)
